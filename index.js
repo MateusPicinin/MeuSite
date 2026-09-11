85b3f18a730d791d366a38eba1ebef445c3caee7
MateusPicinin <picininmateus@gmail.com>
2026-09-11 16:12:18 -0300

    SiteOK

---
+++ index.js
@@ -0,0 +1,61 @@
+const botaoBatalhaNaval = document.getElementById('BtnBatalhaNaval');
+
+botaoBatalhaNaval.addEventListener('click', function() {
+    window.open('https://github.com/MateusPicinin/Projects/tree/main/Python%20Batalha%20Naval', '_blank');
+});
+
+
+const botaoBancoDeDados = document.getElementById('BtnBancoDeDados');
+
+botaoBancoDeDados.addEventListener('click', function() {
+    window.open('https://github.com/MateusPicinin/Projects/tree/main/Banco%20de%20dados', '_blank');
+});
+
+
+
+const botaoMachineLearning = document.getElementById('BtnMachineLearning');
+
+botaoMachineLearning.addEventListener('click', function() {
+    window.open('https://github.com/MateusPicinin/Projects/tree/main/Machine%20Learning', '_blank');
+});
+
+
+
+const botaoJokenpo = document.getElementById('BtnJokenpo');
+
+botaoJokenpo.addEventListener('click', function() {
+    window.open('https://github.com/MateusPicinin/Projects/tree/main/Jokenpo', '_blank');
+});
+
+
+
+
+
+
+
+const botaoGithub = document.getElementById('github');
+
+botaoGithub.addEventListener('click', function() {
+    window.open('https://github.com/MateusPicinin', '_blank');
+});
+
+
+const botaoLinkedin = document.getElementById('linkedin');
+
+botaoLinkedin.addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/mateus-augusto-picinin-5082683b8/', '_blank');
+});
+
+
+const botaoInstagram = document.getElementById('instagram');
+
+botaoInstagram.addEventListener('click', function() {
+    window.open('https://www.instagram.com/mts_picinin/', '_blank');
+});
+
+
+const botaoX = document.getElementById('x');
+
+botaoX.addEventListener('click', function() {
+    window.open('https://x.com/?lang=pt', '_blank');
+});
